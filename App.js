f974e64a486bd6e920e7be0718bf54c669aa542d
rhobbikhaidir <rhobbie09@gmail.com>
2020-10-10 20:40:15 +0700

    Frist Commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [outPutText, SetOutPutText] = useState(
+    "Open up App.js to start working on your app!"
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outPutText}</Text>
+      <Button title="Text Changing" onPress={() => SetOutPutText("Succes")} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
